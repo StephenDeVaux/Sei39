2008e56b64705be289019df936b20ced6aa497d2
Stevey <60765528+StephenDeVaux@users.noreply.github.com>
2020-09-16 11:45:11 +1000

    first prorgams

---
+++ 2-my-first-prog/demo.js
@@ -0,0 +1,29 @@
+// let randomNum = Math.floor(Math.random() * 10)
+// let guess = prompt("Guess a number between 0 and 10")
+
+// console.log(randomNum)
+// console.log(guess)
+
+// while (randomNum !== Number(guess)) {
+//     debugger
+//     if (guess > randomNum) {
+//         guess = prompt("Too high guess again between 0 and 10")
+//     } else {
+//         guess = prompt("Too low guess again between 0 and 10")
+//     }
+// }
+
+// alert("Well done you got it!")
+
+
+let children = 1;
+let partnerName = "Candice";
+let location1 = "Wdonga";
+let title = "Engineer";
+alert("You will be " + title + " in " + location1 + ", and married to " + partnerName + " with " + children + " kids.")
+
+let currentYear = 2020;
+let birthYear = 1989;
+let age1 = currentYear - birthYear;
+let age2 = currentYear - birthYear - 1;
+alert("You are either " + age1 + " or " + age2)
